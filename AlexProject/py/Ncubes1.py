dce73f51fb9e8041c68b192005a9065af9e23235
JW <john@freepower.io>
2023-09-19 18:32:58 -0700

    fix pt bugs

--- AlexProject/py/Ncubes1.py
+++ AlexProject/py/Ncubes1.py
@@ -1,4 +1,5 @@
 import math
+import copy
 import collections
 def specify():
   global PixelsPerInch
@@ -12,12 +13,11 @@ def specify():
   PixelsPerInch =350
   Width= PixelsPerInch * 35.5 
   marginPixels= .25 * PixelsPerInch
-  n_min= 10
-  n_max= 10
-  squish =180
+  n_min= 4
+  n_max= 4
+  squish =math.pi
   stroke= '90ee90'
-  stroke_width= 1.5
-
+  stroke_width= 100
 
 def design (n, s):
   global theta
@@ -26,13 +26,16 @@ def design (n, s):
   global Width
   global user_center
   global odd
+  print (n)
+  print (odd)
+  print (squish)
   theta =squish/(n - odd)
+  print (theta)
   genbasis (n, s)
-  CanvasPixels =Width + ( 2 * marginPixels )
+  CanvasPixels =Width + (2 * marginPixels)
   user_center =CanvasPixels  / 2.0
   print ("paper square side size inches including bleed (margin):", CanvasPixels / PixelsPerInch)
   draw()
-
 
 def genbasis (n, s):
   global basis
@@ -42,102 +45,95 @@ def genbasis (n, s):
   if odd == 1:
     basis[-1][0] = 0
     basis[-1][1] = 0
+  print (basis)
+def ncube (k):
+  global llist
+  k = k-2             
+  llist = [2, 1, -2, -1]   
+  d = 2              
+  while k >= 2: 
+    d = d+2 
+    k = k-2 
+    cube (llist, d) 
 
-
-
+def cube(l, p):
+  global llist
+  global l1
+  l1=[]
+  l1 =copy.deepcopy (l)
+  llist = []
+  llist.append(p)
+  while l1:
+    llist.append(l1.pop(0))
+  l1 = copy.deepcopy(l)
+  llist.append(p-1)
+  while l1:
+    llist.append(l1.pop(0))
+  l1 = copy.deepcopy(l)
+  llist.append(-p)
+  while l1:
+    llist.append(l1.pop(0))
+  l1 = copy.deepcopy(l)
+  llist.append(1-p)
+  llist.append(l1.pop(0))
+  llist.append(p)
+  llist.append(p-1)
+  llist.append(-p)
+  llist.append(1-p)
+  while l1:
+   x = l1.pop(0) 
+   if abs(x) == abs(p-2):                                            
+     llist.append(p) 
+     llist.append(p-1)
+     llist.append(-p)
+     llist.append(1-p)
+     llist.append(x)
+     llist.append(p)
+     llist.append(p-1)
+     llist.append(-p)
+     llist.append(1-p)
+   if abs(x) != abs(p-2): 
+     llist.append(x)
+  
 def draw():
   global n
   global CanvasPixels
-  global CurrentPosition
   global basis
-  global ll
+  global l1
   global llist
-  fname= open("Alex3.svg", w)
-  fname.write ('<?xml version="1.0" standalone="no"?>')
-  fname.write ('<svg width="', CanvasPixels,'" height="', CanvasPixels,'" version="1.1" xmlns="http://www.w3.org/2000/svg">')
-  CurrentPosition [0] = 0
-  CurrentPosition [1] = 0
+  fname= open("Alex3py.svg", 'w')
+  fname.write ('<?xml version="1.0" standalone="no"?>\n')
+  fname.write ('<svg width="'+ str(CanvasPixels) + '" height="' + str(CanvasPixels) + '" version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
+  CurrentPosition = [0,0]
   for i in range (n):
     CurrentPosition[0] += basis[i][0]
     CurrentPosition[1] += basis[i][1]
   CurrentPosition[0] /= 2
   CurrentPosition[1] /= 2
-  fname.write ('<polyline points="', (user_center + CurrentPosition[0]), (user_center + CurrentPosition[1]))
-
-  ll = llist
-  
-  while ll:
-   dimension dequeue "ll
-   direction sign dimension
-   dimension abs dimension
-   itskip 1
-   for [i 1 2 1] [~
-    Current item i CurrentPosition
-    index (list dimension i)
-    delta mditem index basis
-    if not equalp delta 0 [itskip 0]
-    setitem  i CurrentPosition (Current + ( direction * delta )) ]
-   if itskip = 0 [~
-     (print(user_center + item 1 CurrentPosition)( user_center + item 2 CurrentPosition))]
-  ]
-  
-  print ["]
-  type [    stroke="#]
-  type stroke 
-  type  [" fill="none" stroke-width="]
-  type stoke_width
-  type ["/>]
-  print [</svg>]
-  
-  setwrite []
-  show fname
-  show "done
-  close fname
- 
-
-
-def ncube (k):
-  global llist
-  k = k-2             
-  if k < 0:
-    return
-  llist = [2, 1, -2, -1]   
-  d = 2              
-  while k >= 2: 
-    d = d+2 
-    k k-2 
-    cube (llist d) 
-
-
-def cube( l, p)
-l1 = l
-llist = []
-queue "llist p
-while [not l1=[]] [queue "llist dequeue "l1]
-l1 l
-queue "llist p-1
-while [not l1=[]] [queue "llist dequeue "l1]
-l1 l
-queue "llist -p
-while [not l1=[]] [queue "llist dequeue "l1]
-l1 l
-queue "llist 1-p
-queue "llist dequeue "l1
-queue "llist p queue "llist p-1 queue "llist -p queue "llist 1-p
-while [ not l1=[] ] [
- x dequeue "l1 
- if (abs x) = (abs p-2)  [                                          
-   queue "llist p queue "llist p-1 queue "llist -p queue "llist 1-p
-   queue "llist x 
-   queue "llist p queue "llist p-1 queue "llist -p queue "llist 1-p]
- if not  (abs x) = (abs p-2) [ queue "llist x]]
-end
-
+  fname.write ('<polyline points="\n' + str(user_center + CurrentPosition[0]) + ' ' + str(user_center + CurrentPosition[1])+ '\n' )
+  l1 = copy.deepcopy(llist)
+  while l1:
+   dimension = l1.pop(0)
+   direction = math.copysign(1,dimension)
+   dimension = abs(dimension)
+   itskip = 1
+   for i in range(2):
+    Current = CurrentPosition[i]
+    delta = basis[dimension-1][i]
+    if delta != 0:
+     itskip = 0
+    CurrentPosition[i]= (Current + (direction * delta)) 
+   if itskip == 0:
+     fname.write (str(user_center + CurrentPosition[0]) + ' ' + str( user_center + CurrentPosition[1]) + '\n')
+  fname.write('"\nstroke="#' + str(stroke) + '" fill="none" stroke-width="' + str(stroke_width) + '"/>\n')
+  fname.write ('</svg>\n')
+  fname.close()
 specify()
-for n in range (n_min, (n_max + 1), 1): #main loop
+for n in range (n_min, (n_max + 1), 1):
     odd  = n % 2
     s = (Width * math.pi) / (2 * n)
     n= n + odd
     p= n
-    ncube( n)
-    design (n, p, s )
+    ncube(n)
+    design (n, s)
+    
