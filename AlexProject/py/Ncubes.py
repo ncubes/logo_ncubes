65e602d60c2fd40645054ff070ba58180f2fb2aa
JW <john@freepower.io>
2023-09-20 17:01:32 -0700

    simplify

--- AlexProject/py/Ncubes.py
+++ AlexProject/py/Ncubes.py
@@ -14,18 +14,15 @@ def specify():
   Width= PixelsPerInch * 35.5 
   marginPixels= .25 * PixelsPerInch
   n_min= 2
-  n_max= 14
+  n_max= 4
   squish =math.pi
   stroke= '90ee90'
   stroke_width= 100
 
 def design (n, s):
-  global theta
-  global squish
   global CanvasPixels
-  global Width
   global user_center
-  global odd
+  global theta
   theta =squish/(n - odd)
   genbasis (n, s)
   CanvasPixels =Width + (2 * marginPixels)
@@ -35,68 +32,57 @@ def design (n, s):
   
 def genbasis (n, s):
   global basis
-  global theta
-  global CanvasPixels
   basis = [[s * math.cos (i * theta), s * math.sin (i * theta)] for i in range(n)]
   if odd == 1:
     basis[-1][0] = 0
     basis[-1][1] = 0
 
 def ncube (k):
-  global llist
+  global edge
   k = k-2             
-  llist = [2, 1, -2, -1]   
+  edge = [2, 1, -2, -1]   
   d = 2              
   while k >= 2: 
     d = d+2 
     k = k-2 
-    cube (llist, d) 
-
-def cube(l, p):
-  global llist
-  global l1
-  l1=[]
-  l1 =copy.deepcopy (l)
-  llist = []
-  llist.append(p)
-  while l1:
-    llist.append(l1.pop(0))
-  l1 = copy.deepcopy(l)
-  llist.append(p-1)
-  while l1:
-    llist.append(l1.pop(0))
-  l1 = copy.deepcopy(l)
-  llist.append(-p)
-  while l1:
-    llist.append(l1.pop(0))
-  l1 = copy.deepcopy(l)
-  llist.append(1-p)
-  llist.append(l1.pop(0))
-  llist.append(p)
-  llist.append(p-1)
-  llist.append(-p)
-  llist.append(1-p)
-  while l1:
-   x = l1.pop(0) 
-   if abs(x) == abs(p-2):                                            
-     llist.append(p) 
-     llist.append(p-1)
-     llist.append(-p)
-     llist.append(1-p)
-     llist.append(x)
-     llist.append(p)
-     llist.append(p-1)
-     llist.append(-p)
-     llist.append(1-p)
-   if abs(x) != abs(p-2): 
-     llist.append(x)
+    l_prime=[]
+    l_prime =copy.deepcopy (edge)
+    edge = []
+    edge.append(d)
+    while l_prime:
+      edge.append(l_prime.pop(0))
+    l_prime = copy.deepcopy(edge)
+    edge.append(d-1)
+    while l_prime:
+      edge.append(l_prime.pop(0))
+    l_prime = copy.deepcopy(edge)
+    edge.append(-d)
+    while l_prime:
+      edge.append(l_prime.pop(0))
+    l_prime = copy.deepcopy(edge)
+    edge.append(1-d)
+    edge.append(l_prime.pop(0))
+    edge.append(d)
+    edge.append(d-1)
+    edge.append(-d)
+    edge.append(1-d)
+    while l_prime:
+     x = l_prime.pop(0) 
+     if abs(x) == abs(d-2):                                            
+       edge.append(d) 
+       edge.append(d-1)
+       edge.append(-d)
+       edge.append(1-d)
+       edge.append(x)
+       edge.append(d)
+       edge.append(d-1)
+       edge.append(-d)
+       edge.append(1-d)
+     if abs(x) != abs(d-2): 
+       edge.append(x)
   
 def draw():
-  global n
-  global CanvasPixels
-  global basis
-  global l1
-  global llist
+  global edge
   fname= open(str(n-odd)+"cube.svg", 'w')
   fname.write ('<?xml version="1.0" standalone="no"?>\n')
   fname.write ('<svg width="'+ str(CanvasPixels) + '" height="' + str(CanvasPixels) + '" version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
@@ -107,9 +93,9 @@ def draw():
   CurrentPosition[0] /= -2
   CurrentPosition[1] /= -2
   fname.write ('<polyline points="\n' + str(user_center + CurrentPosition[0]) + ' ' + str(user_center + CurrentPosition[1])+ '\n' )
-  l1 = copy.deepcopy(llist)
-  while l1:
-   dimension = l1.pop(0)
+  l_p = copy.deepcopy(edge)
+  while l_p:
+   dimension = l_p.pop(0)
    direction = math.copysign(1,dimension)
    dimension = abs(dimension)
    itskip = 1
